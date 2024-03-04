2028446fac42544621e114b44788b9fb649c102e
bunsbof <nguyenminhcuong2000@gmail.com>
2024-03-04 22:14:48 +0700

    #2024/03/04 - Autoload variables

--- scenes/pipes/pipes.gd
+++ scenes/pipes/pipes.gd
@@ -15,5 +15,15 @@ func _process(delta):
 
 
 func _on_screen_exited():
-	print("screen_exited")
+	#print("screen_exited")
 	queue_free()
+
+
+func _on_pipe_body_entered(body):
+	if body.is_in_group(GameManager.GROUP_PLAYER)==true:
+		print("_on_pipe_body_entered ", body)
+
+
+func _on_laser_body_exited(body):
+	if body.is_in_group(GameManager.GROUP_PLAYER)==true:	
+		print("_on_laser_body_exited ", body)
